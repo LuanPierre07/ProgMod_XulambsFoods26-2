41137ff0a04a187bb1d282c0dfd1bdf576075132
Luan Pierre Teixeira Soares <pierreluan27@gmail.com>
2026-08-31 11:54:42 -0300

    Incluidos teste para pizza

--- src/PizzaTest.java
+++ src/PizzaTest.java
@@ -1,20 +1,59 @@
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PizzaTest {
 
+    Pizza pizza;
+
+    @BeforeEach
+    public void setUp(){
+        //Arrange
+        pizza = new Pizza();
+        pizza.adicionarIngredientes(2);
+    }
+
     @Test
     public void adicionaIngredientesCorretamente(){
-        //Arrange
-        Pizza pizza = new Pizza();
 
         //Act
-        int quantos = 
-            pizza.adicionarIngredientes(4);
+        int quantos = pizza.adicionarIngredientes(4);
 
         //Assert
-        assertEquals(4, quantos);
+        assertEquals(6, quantos);
+    }
+
+    @Test
+    public void naoAdicionaIngredienteNegativo(){
+
+        //Act
+        int quantos =
+            pizza.adicionarIngredientes(-7);
+
+        //Assert:
+        assertEquals(2, quantos);
+    }
+
+    @Test public void naoUltrapassaMaximoDeIngredientes(){
+
+        //Act
+        int quantos = pizza.adicionarIngredientes(7);
+
+        //Assert
+        assertEquals(2, quantos);
+
+    }
+
+    @Test
+    public void calcularOPrecoCorretamente(){
+
+        //Act
+        double preco = pizza.valorFinal();
+
+        //Assert
+        assertEquals(39, preco, 0.01);
     }
     
 }
