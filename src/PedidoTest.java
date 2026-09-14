f113a12c1b7b1b76f4a420b43eb7eda34e0921d0
Luan Pierre Teixeira Soares <pierreluan27@gmail.com>
2026-09-14 12:00:22 -0300

    completado teste pedido

--- src/PedidoTest.java
+++ src/PedidoTest.java
@@ -1,14 +1,35 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PedidoTest {
+
+    Pedido pedido;
+    Pizza pizzavazia;
+
+    @BeforeEach
+    public void setUp(){
+        pedido = new Pedido();
+        pizzavazia = new Pizza();
+        pedido.adicionarPizza(pizzavazia); 
+    }
+
+    @Test
+    public void adicionaVariasPizzasCorretamente(){
+        //Arrange
+
+        //Act
+            int quantidade = pedido.adicionarPizza(new Pizza());
+
+        //Assert
+        assertEquals(2,quantidade);
+    }
     
     @Test
     public void naoAdicionaPizzaEmPedidoFechado(){
         //Arrange
-        Pedido pedido = new Pedido();
-        pedido.adicionarPizza(new Pizza());
         pedido.fecharPedido();
 
         //Act
@@ -16,5 +37,42 @@ public class PedidoTest {
     
         //Assert
         assertEquals(1, quantidade);
+    }
+
+    @Test
+    public void calculaValorDoPedidoCorretamente(){
+        //Arrange
+
+        //Act
+        double preco = pedido.precoAPagar();
+    
+        //Assert
+        assertEquals(29d, preco, 0.01);
+    }
+
+    @Test 
+    public void calculaPrecoPedidoVariasPizzas(){
+        //Arrange
+        Pizza pizza2ingrediente = new Pizza(2);
+        pedido.adicionarPizza(pizza2ingrediente);
+
+        //Act
+        double preco = pedido.precoAPagar();
+
+        //Assert
+        assertEquals(68d, preco, 0.01);
+    }
+
+    @Test 
+    public void geraRelatorioPedido(){
+        //Act
+        String cupom = pedido.relatorio();
+
+        //Assert
+        assertTrue(
+            cupom.contains("29,00") &&
+            cupom.contains("1 pizzas") &&
+            cupom.contains("aberto")
+        );
     }
 }
